e3e40562b31e29c1c5f651f86be79a09345b3d17
unknown <justvcreative@gmail.com>
2016-06-24 21:18:11 -0400

    First Commit

---
+++ inc/header.php
@@ -0,0 +1,19 @@
+<?php
+    echo "Hello PHP World!";
+?>
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags always come first -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta http-equiv="x-ua-compatible" content="ie=edge">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.2/css/bootstrap.min.css" integrity="sha384-y3tfxAZXuh4HwSYylfB+J125MxIs6mR5FOHamPBG064zB+AFeWH94NdvaCBm8qnd" crossorigin="anonymous">
+  	<link rel="stylesheet" type="text/css" href="css/main.css">
+  </head>
+  <body>
+    <div class="container">
+        <span id="logo" class="m-x-auto"><a href="#"><img class="img-fluid" src="img/logo.png"></a></span>
+        <div class="m-x-auto">Centered block</div>
---
+++ index.php
@@ -0,0 +1,14 @@
+<?php
+    include("inc/header.php");
+    $title = "Monster Gallery";
+?>
+    <section id="gallery">
+    	<h1 id="title" class="center-block"><?php echo $title; ?></h1>
+    </section>
+</div><!-- End Content Container -->
+    <!-- jQuery first, then Bootstrap JS. -->
+    <script src="https://www.atlasestateagents.co.uk/javascript/tether.min.js"></script><!-- Tether for Bootstrap -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.4/jquery.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.2/js/bootstrap.min.js" integrity="sha384-vZ2WRJMwsjRMW/8U7i6PWi6AlO1L79snBrmgiDpgIWJ82z8eA5lenwvxbMV1PAh7" crossorigin="anonymous"></script>
+  </body>
+</html>
